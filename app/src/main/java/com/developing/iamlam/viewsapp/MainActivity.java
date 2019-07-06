63ade21c0a7aac31797e4496759c1804b8f0f371
IamLAM <ingeniero@luismonge.com.mx>
2019-07-05 23:23:27 -0700

    base project

---
+++ app/src/main/java/com/developing/iamlam/viewsapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.developing.iamlam.viewsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
