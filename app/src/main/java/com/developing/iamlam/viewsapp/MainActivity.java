7da665d489a6a4c40cf6d57c52becdbfffb697a4
IamLAM <ingeniero@luismonge.com.mx>
2019-07-11 01:56:18 -0700

    id asigned to textview

--- app/src/main/java/com/developing/iamlam/viewsapp/MainActivity.java
+++ app/src/main/java/com/developing/iamlam/viewsapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.developing.iamlam.viewsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView answerText=findViewById(R.id.answer);
+        answerText.setText("La edad es:");
     }
 }
