c18004facbc58b64a09b81fe69226c307061c50f
IamLAM <ingeniero@luismonge.com.mx>
2019-07-12 23:33:01 -0700

    Received an answer and convert to integer

--- app/src/main/java/com/developing/iamlam/viewsapp/MainActivity.java
+++ app/src/main/java/com/developing/iamlam/viewsapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,12 +14,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView answerText=findViewById(R.id.answer);
+        final TextView answerText=findViewById(R.id.answer);
+        final EditText ageText=findViewById(R.id.age);
 
         Button buttonAction=findViewById(R.id.button);
         buttonAction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String userAge=ageText.toString();
+                int dogAge=Integer.parseInt(userAge);
+                answerText.setText("Mi edad es "+dogAge);
+
             }
         });
     }
